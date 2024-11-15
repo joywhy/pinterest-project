fb0171e82b981778fd40169b78db2b7fc8632c95
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-11-15 11:30:23 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './views/Home';
+import Bookmark from './views/Bookmark';
+
 function App() {
-  return <div>APP</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bookmark" element={<Bookmark />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
