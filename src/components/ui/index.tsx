e46ff9ae574ce4eef2cf2091be0f02b88988535e
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-11-15 11:15:53 +0900

    feat:경로 index.tsx

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,24 @@
+export { Avatar, AvatarImage, AvatarFallback } from './avatar';
+export { Button, buttonVariants } from './button';
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from './card';
+
+export { Input } from './input';
+export {
+  Pagination,
+  PaginationContent,
+  PaginationLink,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationNext,
+  PaginationEllipsis,
+} from './pagination';
+export { Separator } from './separator';
+
+export { Skeleton } from './skeleton';
